1e8e34e512cc233b8f9288f58d194ec92c3c1faf
Andreia Dias <andreiadias2311@gmail.com>
2025-09-17 17:04:08 -0300

    Playlist_e_NotaMedia.py

---
+++ Playlist_e_NotaMedia.py
@@ -0,0 +1,11 @@
+# crie uma lista de musicas use um loop for para imprimir cada musica da playlist
+
+playlist_musica = ["Imagine", "dandandan", "seila", "outra musica"]
+for i in playlist_musica:
+    print(i)
+
+
+# crie uma lista de notas de um aluno. Calcule a média das notas e adicione o valor da média
+nota_aluno = [0,1,2,3,4,5]
+media = sum(nota_aluno)/len(nota_aluno)
+print(media)
